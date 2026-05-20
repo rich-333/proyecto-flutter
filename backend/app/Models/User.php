348e39aca158f3121728ca544548d9f9cb108e82
richard <interriky@gmail.com>
2026-05-19 22:17:48 -0400

    Agregar controladores de autenticación para choferes y pasajeros, y actualizar modelo de usuario con nuevos campos

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -10,8 +10,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable([
+    'role', 
+    'full_name', 
+    'ci', 
+    'phone', 
+    'email', 
+    'password', 
+    'driver_code', 
+    'pin', 
+    'active'
+])]
+#[Hidden(['password', 'pin', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
@@ -25,8 +35,9 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'pin' => 'hashed', // Opcional: si deseas que el pin también se encripte automáticamente
+            'active' => 'boolean',
         ];
     }
 }
